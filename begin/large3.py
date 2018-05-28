eddcb424794b285a1059e75ad9db0b8856ea5acf
NethajiNallathambi <36121074+NethajiNallathambi@users.noreply.github.com>
2018-05-28 10:55:59 +0530

    Update large3.py

--- begin/large3.py
+++ begin/large3.py
@@ -1,6 +1,6 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
+num1 = (input("Enter first number: "))
+num2 = (input("Enter second number: "))
+num3 = (input("Enter third number: "))
  
 if (num1 > num2) and (num1 > num3):
    largest = num1
